8e6fa1a20d37dd1d5d6b7459e34c6135eeb52d84
ANPULI <al4902@nyu.edu>
2020-01-23 08:01:40 +0800

    attempt

--- api/index.py
+++ api/index.py
@@ -4,9 +4,8 @@ from zhconv import convert
 import pandas as pd
 import re
 import json
-from sanic import Sanic
-from sanic.response import json
-app = Sanic()
+from flask import Flask, Response
+app = Flask(__name__)
 
 
 
@@ -71,8 +70,8 @@ def dict_to_json(confirm_dict):
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-async def index(request, path=""):
-    return json({'hello': path})
+def catch_all(path):
+    return Response("<h1>Flask on ZEIT Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
     # get page
     wiki_url = """https://zh.wikipedia.org/wiki/2019年%EF%BC%8D2020年新型冠狀病毒肺炎事件"""
     soup = BeautifulSoup(get_page(wiki_url), 'lxml')
